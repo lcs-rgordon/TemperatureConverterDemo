a39f2d837767ba332080ce5a91bbbf6207dfaff4
Russell Gordon <rgordon@lcs.on.ca>
2025-02-24 12:24:27 -0500

    Initial Commit

---
+++ TemperatureConverter/TemperatureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterApp.swift
+//  TemperatureConverter
+//
+//  Created by Russell Gordon on 2025-02-24.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
